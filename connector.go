8e5843e812508c1de3fce59366aaa06b45b82aae
Jim Ancona <jim@anconafamily.com>
2020-12-03 21:03:44 -0500

    Multi-receiver WIP

--- connector.go
+++ connector.go
@@ -1,3 +1,5 @@
+// +build !windows
+
 // Copyright 2020 James P. Ancona
 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,10 +29,12 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/hashicorp/logutils"
 	"github.com/jancona/hpsdr"
+	"github.com/jancona/hpsdr/protocol1"
 )
 
 const bufferSize = 2048
@@ -43,6 +47,7 @@ var (
 	controlPort *uint   = flag.Uint("control", 0, "control socket port (default disabled)")
 	radioIP     *string = flag.String("radio", "", "IP address of radio (default use first radio discovered)")
 	isDebug     *bool   = flag.Bool("debug", false, "Emit debug log messages on stdout")
+	isServer    *bool   = flag.Bool("server", false, "Run as the server process for multiple receivers")
 )
 
 func init() {
@@ -69,6 +74,30 @@ func main() {
 	}
 	log.SetOutput(filter)
 	log.Print("[DEBUG] Debug is on")
+	/*
+		struct args {
+			be_a_server     bool
+			iq_port         int
+			control_port    int
+			frequency       int
+			// ...
+		}
+		args = parse_arguments()
+		server_control_port = check_if_server_exists()
+		if args.be_a_server && server_control_port != NULL {
+			// there's already a server
+			exit()
+		}
+		if args.be_a_server {
+			run_as_a_server() // This function terminates when the last receiver is closed
+			exit()
+		}
+		// we're not a server and a server exists
+		control_conn = connect(server_control_port)
+		control_conn.write("add_receiver:"+args.control_port+":"+args.iq_port+":"+args.frequency)
+		// We're done--everything else is done by the server
+		// either exit or wait around to be killed (whichever makes OpenWebRX happy)
+	*/
 
 	switch *sampleRate {
 	case 48000:
@@ -102,29 +131,70 @@ func main() {
 		}
 		addr = devices[0].Network.Address
 	}
-	r := hpsdr.NewProtocol1Radio(addr)
+	r := protocol1.NewRadio(addr)
 	r.SetSampleRate(*sampleRate)
 	r.SetTXFrequency(*frequency)
-	r.SetRX1Frequency(*frequency)
-	r.SetReceiveLNAGain(*lnaGain)
-	var controlConn, iqConn net.Conn
-	distributor := NewDistributor()
+	r.SetLNAGain(*lnaGain)
+	addReceiver(r, *iqPort, *controlPort, *frequency)
 
-	if *controlPort > 0 {
-		log.Printf("[INFO] Listening on control port %d", *controlPort)
+	log.Printf("[INFO] Starting radio on %s", addr.String())
+	err = r.Start()
+	if err != nil {
+		log.Fatalf("Error starting radio %v: %v", r, err)
+	}
+	go sendTransmitSamples(r)
+
+	// wait for a close signal then clean up
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan struct{})
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		<-signalChan
+		log.Print("Received an interrupt, stopping...")
+		r.Close()
+		// if controlConn != nil {
+		// 	controlConn.Close()
+		// }
+		// if iqConn != nil {
+		// 	iqConn.Close()
+		// }
+		close(cleanupDone)
+	}()
+	<-cleanupDone
+}
+
+func newReuseAddrListenConfig() net.ListenConfig {
+	return net.ListenConfig{
+		Control: func(network, address string, c syscall.RawConn) error {
+			return c.Control(func(descriptor uintptr) {
+				syscall.SetsockoptInt(int(descriptor), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+			})
+		},
+	}
+}
+
+func addReceiver(r hpsdr.Radio, iqPort uint, controlPort uint, frequency uint) {
+	distributor := NewDistributor()
+	rec := r.AddReceiver(distributor.Distribute)
+	rec.SetFrequency(frequency)
+	var controlConn, iqConn net.Conn
+
+	if controlPort > 0 {
+		log.Printf("[INFO] Listening on control port %d", controlPort)
 		config := newReuseAddrListenConfig()
-		controlListener, err := config.Listen(context.Background(), "tcp", fmt.Sprintf("127.0.0.1:%d", *controlPort))
+		controlListener, err := config.Listen(context.Background(), "tcp", fmt.Sprintf("127.0.0.1:%d", controlPort))
 		if err != nil {
-			log.Fatalf("Error listening on control port %d: %v", *controlPort, err)
+			log.Fatalf("Error listening on control port %d: %v", controlPort, err)
 		}
 		go func() {
 			for {
-				log.Printf("[DEBUG] Calling Accept on control port %d", *controlPort)
+				log.Printf("[DEBUG] Calling Accept on control port %d", controlPort)
 				controlConn, err = controlListener.Accept()
 				if err != nil {
-					log.Fatalf("Error accepting connection on control port %d: %v", *controlPort, err)
+					// TODO: How should we handle this?
+					log.Fatalf("Error accepting connection on control port %d: %v", controlPort, err)
 				}
-				log.Printf("[DEBUG] Opened connection on control port %d", *controlPort)
+				log.Printf("[DEBUG] Opened connection on control port %d", controlPort)
 				buf := make([]byte, 128)
 				for run := true; run; {
 					cnt, err := controlConn.Read(buf)
@@ -137,13 +207,14 @@ func main() {
 						run = false
 						break
 					}
-					cmd := strings.Trim(string(buf[:cnt]), "\x00\n ")
+					cmd := strings.Trim(string(buf[:cnt]), "\x00\r\n ")
 					log.Printf("[DEBUG] Received command '%s'", cmd)
 					tok := strings.Split(cmd, ":")
-					if len(tok) != 2 {
-						log.Printf("[DEBUG] Ignoring invalid command '%s'", cmd)
-						continue
-					}
+					log.Printf("[DEBUG] len(tok)=%d", len(tok))
+					// if len(tok) != 2 {
+					// 	log.Printf("[DEBUG] Ignoring invalid command '%s'", cmd)
+					// 	continue
+					// }
 					switch tok[0] {
 					case "samp_rate":
 						sr, err := strconv.ParseUint(tok[1], 10, 64)
@@ -155,12 +226,12 @@ func main() {
 					case "center_freq":
 						f, err := strconv.ParseUint(tok[1], 10, 64)
 						if err != nil {
-							log.Printf("[DEBUG] Ignoring bad center frrequency value in control command '%s': %v", cmd, err)
+							log.Printf("[DEBUG] Ignoring bad center frequency value in control command '%s': %v", cmd, err)
 						}
-						*frequency = uint(f)
-						r.SetTXFrequency(*frequency)
-						r.SetRX1Frequency(*frequency)
-						if *frequency >= 3000000 {
+						frequency = uint(f)
+						r.SetTXFrequency(frequency)
+						rec.SetFrequency(frequency)
+						if frequency >= 3000000 {
 							// Turn on high pass filter
 							r.SetOCOut(0b1000000)
 						} else {
@@ -177,7 +248,24 @@ func main() {
 							}
 							*lnaGain = uint(gain)
 						}
-						r.SetReceiveLNAGain(*lnaGain)
+						r.SetLNAGain(*lnaGain)
+					case "new_receiver":
+						if len(tok) != 4 {
+							log.Printf("[DEBUG] Ignoring bad new_receiver command: %s", cmd)
+						}
+						iqPort, err := strconv.ParseUint(tok[1], 10, 64)
+						if err != nil {
+							log.Printf("[DEBUG] Ignoring bad IQ port value in control command '%s': %v", cmd, err)
+						}
+						controlPort, err := strconv.ParseUint(tok[2], 10, 64)
+						if err != nil {
+							log.Printf("[DEBUG] Ignoring bad control port value in control command '%s': %v", cmd, err)
+						}
+						frequency, err := strconv.ParseUint(tok[3], 10, 64)
+						if err != nil {
+							log.Printf("[DEBUG] Ignoring bad frequency value in control command '%s': %v", cmd, err)
+						}
+						addReceiver(r, uint(iqPort), uint(controlPort), uint(frequency))
 					default:
 						log.Printf("[DEBUG] Ignoring unsupported control command: %s", cmd)
 					}
@@ -186,31 +274,31 @@ func main() {
 		}()
 	}
 
-	log.Printf("[INFO] Listening on IQ port %d", *iqPort)
+	log.Printf("[INFO] Listening on IQ port %d", iqPort)
 	config := newReuseAddrListenConfig()
-	iqListener, err := config.Listen(context.Background(), "tcp", fmt.Sprintf("127.0.0.1:%d", *iqPort))
+	iqListener, err := config.Listen(context.Background(), "tcp", fmt.Sprintf("127.0.0.1:%d", iqPort))
 	if err != nil {
-		log.Fatalf("Error listening on port %d: %v", *iqPort, err)
+		log.Fatalf("Error listening on port %d: %v", iqPort, err)
 	}
 	go func() {
 		for {
-			log.Printf("[DEBUG] Calling Accept on IQ port %d", *iqPort)
+			log.Printf("[DEBUG] Calling Accept on IQ port %d", iqPort)
 			iqConn, err = iqListener.Accept()
 			if err != nil {
-				log.Fatalf("Error accepting connection on port %d: %v", *iqPort, err)
+				log.Fatalf("Error accepting connection on port %d: %v", iqPort, err)
 			}
-			log.Printf("[DEBUG] Opened connection on IQ port %d", *iqPort)
+			log.Printf("[DEBUG] Opened connection on IQ port %d", iqPort)
 			go func(conn net.Conn) {
 				sampleChan := distributor.Listen()
 				defer distributor.Close(sampleChan)
 				defer func() {
-					log.Printf("[DEBUG] Closing IQ socket on port %d", *iqPort)
+					log.Printf("[DEBUG] Closing IQ socket on port %d", iqPort)
 					err = conn.Close()
 					if err != nil {
-						log.Printf("[DEBUG] Error closing IQ socket on port %d: %v", *iqPort, err)
+						log.Printf("[DEBUG] Error closing IQ socket on port %d: %v", iqPort, err)
 					}
 				}()
-				var samples []hpsdr.ReceiverSample
+				var samples []hpsdr.ReceiveSample
 				for run := true; run; {
 					samples, run = <-sampleChan
 					if run {
@@ -221,7 +309,7 @@ func main() {
 						}
 						_, err := conn.Write(buf)
 						if err != nil {
-							log.Printf("[DEBUG] Error writing to IQ port %d: %v", *iqPort, err)
+							log.Printf("[DEBUG] Error writing to IQ port %d: %v", iqPort, err)
 							run = false
 						}
 					}
@@ -229,59 +317,33 @@ func main() {
 			}(iqConn)
 		}
 	}()
-
-	log.Printf("[INFO] Starting radio on %s", addr.String())
-	err = r.Start()
-	if err != nil {
-		log.Fatalf("Error starting radio %v: %v", *r, err)
-	}
-	go distributor.Distribute(r)
-	go sendTransmitSamples(r)
-
-	// wait for a close signal then clean up
-	signalChan := make(chan os.Signal, 1)
-	cleanupDone := make(chan struct{})
-	signal.Notify(signalChan, os.Interrupt)
-	go func() {
-		<-signalChan
-		log.Print("Received an interrupt, stopping...")
-		r.Close()
-		if controlConn != nil {
-			controlConn.Close()
-		}
-		if iqConn != nil {
-			iqConn.Close()
-		}
-		close(cleanupDone)
-	}()
-	<-cleanupDone
 }
 
-func sendTransmitSamples(r *hpsdr.Protocol1Radio) {
-	ts := make([]hpsdr.TransmitSample, hpsdr.TransmitSamplesPerMessage)
+func sendTransmitSamples(r hpsdr.Radio) {
+	ts := make([]hpsdr.TransmitSample, r.TransmitSamplesPerMessage())
 	for {
 		// Send empty transmit samples to pass config changes and keep watchdog timer happy
 		r.SendSamples(ts)
-		time.Sleep(time.Second / (48000 / hpsdr.TransmitSamplesPerMessage))
+		time.Sleep(time.Second / time.Duration(48000/r.TransmitSamplesPerMessage()))
 	}
 }
 
 // Distributor distributes received samples to one or more IQ goroutines
 type Distributor struct {
 	sync.RWMutex
-	listeners map[chan []hpsdr.ReceiverSample]bool
+	listeners map[chan []hpsdr.ReceiveSample]bool
 }
 
 // NewDistributor constructs a distributor
 func NewDistributor() Distributor {
 	return Distributor{
-		listeners: map[chan []hpsdr.ReceiverSample]bool{},
+		listeners: map[chan []hpsdr.ReceiveSample]bool{},
 	}
 }
 
 // Listen returms a channel for a listening goroutine to receive samples
-func (d *Distributor) Listen() chan []hpsdr.ReceiverSample {
-	c := make(chan []hpsdr.ReceiverSample, *sampleRate) // one second buffer
+func (d *Distributor) Listen() chan []hpsdr.ReceiveSample {
+	c := make(chan []hpsdr.ReceiveSample, *sampleRate) // one second buffer
 	d.Lock()
 	d.listeners[c] = true
 	d.Unlock()
@@ -290,7 +352,7 @@ func (d *Distributor) Listen() chan []hpsdr.ReceiverSample {
 }
 
 // Close closes a channel when a goroutine is done
-func (d *Distributor) Close(c chan []hpsdr.ReceiverSample) {
+func (d *Distributor) Close(c chan []hpsdr.ReceiveSample) {
 	log.Printf("[DEBUG] Removing listener %v", c)
 	d.Lock()
 	delete(d.listeners, c)
@@ -300,22 +362,16 @@ func (d *Distributor) Close(c chan []hpsdr.ReceiverSample) {
 }
 
 // Distribute receives samples from a radio and distributes them to listening goroutines
-func (d *Distributor) Distribute(r *hpsdr.Protocol1Radio) {
-	for {
-		r.ReceiveSamples(
-			func(r *hpsdr.Protocol1Radio, samples []hpsdr.ReceiverSample) {
-				// log.Printf("[DEBUG] Got samples %#v", samples)
-				d.RLock()
-				for sampleChan := range d.listeners {
-					select {
-					case sampleChan <- samples:
-						// fmt.Println("sent message")
-					default:
-						// Drop unsent samples on the floor
-					}
-				}
-				d.RUnlock()
-			})
+func (d *Distributor) Distribute(samples []hpsdr.ReceiveSample) {
+	// log.Printf("[DEBUG] Got samples %#v", samples)
+	d.RLock()
+	for l := range d.listeners {
+		select {
+		case l <- samples:
+			// fmt.Println("sent message")
+		default:
+			// Drop unsent samples on the floor
+		}
 	}
-
+	d.RUnlock()
 }
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/hashicorp/logutils v1.0.0
 	github.com/jancona/hpsdr v0.2.0
 )
+
+// replace github.com/jancona/hpsdr => /home/jim/hpsdr
